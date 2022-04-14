b3a731c205fa296de9d40e9ee5adfa162e1b0dfe
James <james.auble@gmail.com>
2022-04-14 09:17:13 -0700

    lowercased components

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import parse from "html-react-parser"
 
-import Bio from "../components/Bio"
-import Layout from "../components/Layout"
-import Seo from "../components/SEO"
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const BlogIndex = ({
   data,
